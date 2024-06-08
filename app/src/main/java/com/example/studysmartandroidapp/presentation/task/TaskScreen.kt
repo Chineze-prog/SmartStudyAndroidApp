cb1880155e4a1b462da66051b50ce0229d215ccd
Chineze-prog <chinezeobi.bomi@gmail.com>
2024-06-07 20:18:43 -0600

    building the Task Screen UI

--- app/src/main/java/com/example/studysmartandroidapp/presentation/task/TaskScreen.kt
+++ app/src/main/java/com/example/studysmartandroidapp/presentation/task/TaskScreen.kt
@@ -1,13 +1,26 @@
 package com.example.studysmartandroidapp.presentation.task
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -22,6 +35,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -29,6 +43,7 @@ import com.example.studysmartandroidapp.presentation.components.DeleteDialogue
 import com.example.studysmartandroidapp.presentation.components.TaskCheckBox
 import com.example.studysmartandroidapp.presentation.domain.model.Task
 import com.example.studysmartandroidapp.presentation.theme.Red
+import com.example.studysmartandroidapp.presentation.utils.Priority
 
 @Composable
 fun TaskScreen(
@@ -72,6 +87,7 @@ fun TaskScreen(
     ){ paddingValue ->
         Column(
             modifier = Modifier
+                .verticalScroll(state = rememberScrollState())
                 .fillMaxSize()
                 .padding(paddingValue)
                 .padding(horizontal = 12.dp)
@@ -94,7 +110,90 @@ fun TaskScreen(
                 //singleLine = true, so the text field can have multiple lines
             )
 
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = "Due Date",
+                style = MaterialTheme.typography.bodySmall
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    text = "07 June 2024",
+                    style = MaterialTheme.typography.bodyLarge
+                )
+
+                IconButton(onClick = { /*TODO*/ } ) {
+                    Icon(
+                        imageVector = Icons.Default.DateRange,
+                        contentDescription = "Select Due Date"
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = "Priority",
+                style = MaterialTheme.typography.bodySmall
+            )
+
             Spacer(modifier = Modifier.height(10.dp))
+
+            Row(modifier = Modifier.fillMaxWidth()){
+                Priority.entries.forEach { priority ->
+                    PriorityButton(
+                        modifier = Modifier.weight(1f), //so the three buttons are the same size
+                        label = priority.title,
+                        backgroundColor = priority.color,
+                        //will change once view model is implemented
+                        borderColor = if (priority == Priority.MEDIUM) { Color.White }
+                        else Color.Transparent,
+                        //will change once view model is implemented
+                        labelColor = if (priority == Priority.MEDIUM) { Color.White }
+                        else Color.White.copy(alpha = 0.7f),
+                        onPriorityButtonClick = { /*TODO*/ }
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text(
+                text = "Related to subject",
+                style = MaterialTheme.typography.bodySmall
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    text = "English",
+                    style = MaterialTheme.typography.bodyLarge
+                )
+
+                IconButton(onClick = { /*TODO*/ } ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Select related subject"
+                    )
+                }
+            }
+            Button(
+                enabled = taskTitleError == null,
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
+            ){
+                Text(text = "Save")
+            }
         }
     }
 }
@@ -142,4 +241,26 @@ private fun TaskScreenTopBar(
             }
         }
     )
+}
+
+@Composable
+private fun PriorityButton(
+    modifier: Modifier = Modifier,
+    label: String,
+    backgroundColor: Color,
+    borderColor: Color,
+    labelColor: Color,
+    onPriorityButtonClick: () -> Unit
+){
+    Box(
+        modifier = modifier
+            .background(backgroundColor)
+            .clickable { onPriorityButtonClick() }
+            .padding(5.dp) //padding b/w the background and the border
+            .border(1.dp, borderColor, RoundedCornerShape(5.dp))
+            .padding(5.dp), //padding between the border and the label
+        contentAlignment = Alignment.Center
+    ){
+        Text(text = label, color = labelColor)
+    }
 }
