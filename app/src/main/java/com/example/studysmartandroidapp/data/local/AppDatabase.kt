77027058db1a1bb25e0149db0e317c98020f704f
Chineze-prog <chinezeobi.bomi@gmail.com>
2024-06-17 12:40:42 -0600

    implementing app database using Room

---
+++ app/src/main/java/com/example/studysmartandroidapp/data/local/AppDatabase.kt
@@ -0,0 +1,23 @@
+package com.example.studysmartandroidapp.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.studysmartandroidapp.domain.model.Session
+import com.example.studysmartandroidapp.domain.model.Subject
+import com.example.studysmartandroidapp.domain.model.Task
+
+@Database(
+    entities = [Subject::class, Task::class, Session::class],
+    version = 1 //if the schema is changed in the future, this needs to be updated
+)
+
+@TypeConverters(ColorListConverter::class)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun subjectDao(): SubjectDao
+
+    abstract fun taskDao(): TaskDao
+
+    abstract fun sessionDao(): SessionDao
+
+}
